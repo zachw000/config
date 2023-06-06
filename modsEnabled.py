24fb7e46cf40390eebaa33305f7b79f4bccca600
zachw000 <waldrzac000@gmail.com>
2023-06-06 00:19:29 -0700

    Implemented Reload fixed exit command

--- modsEnabled.py
+++ modsEnabled.py
@@ -3,6 +3,7 @@ EFLG = 0
 
 def tokenize(command):
     tokens = command.split(' ')
+    return tokens
 
 def readMods():
     # Opening JSON file
@@ -23,31 +24,36 @@ def readMods():
     # Closing file
     f.close()
     return modlist
+def processTokens(tokens):
+    return tokens
 def commandLoop(mods):
     # Error flag, by default will assume error
     EFLG = 0b1111111111111111
     cmd = ""
-    while cmd != "exit" or cmd != "stop":
+    while cmd != "exit" or cmd != "stop" or cmd != "end":
         cmd = input("MODLIST $ ").lower()
-        if cmd == "exit":
-            EFLG = EFLG & ~EFLG
+        if cmd == "exit" or cmd == "stop" or cmd == "end" or cmd is None:
+            EFLG = 0b00000
+            break
         elif len(tokenize(cmd)) > 1:
             # command has multiple arguments
-            EFLG = 0b0000000000000000
-            tokenize(cmd)
+            EFLG = 0x0
+            tkns = tokenize(cmd)
+            processTokens(tkns)
         else:
             EFLG = 0b1111000010110001
             if cmd == "help" or cmd == "list" or cmd == "reload" or cmd == "refresh":
-                EFLG = 0xA
+                EFLG = 0b1010   # 0xA
                 if cmd == "list":
                     for i in mods:
                         # print u'\u2713' Prints check mark 'approval'
+                        print('\t' + str(i['modName']) + "\n\t\tEnabled?  " + str(i['enabled']) + '\t\t' , end="")
                         if i['enabled']:
-                            print(str(i['modName']) + "\nEnabled?  " + str(i['enabled']) + '\t\t\t' + u'\u2713')
+                            print(u'\u2713\n', end="")
                         else:
-                            print(str(i['modName']) + "\nEnabled?  " + str(i['enabled']) + '\t\t\t' + u'\u2717')
+                            print(u'\u2717\n', end="")
                 elif cmd == "reload" or cmd == "refresh":
-                    EFLG = 0xB
+                    EFLG = 0b1011 # 0xB
                     mods = readMods()
                 elif cmd == "help":
                     print("Usage MODLIST $ <help|list|refresh|enable|disable|add|remove>")
